234037c352d9c108f47a515a130ee3ba786f8d06
younghwan <younghwan@github.com>
2025-08-10 03:16:00 +0900

    update cardview

--- ReleaeSpoon/app/src/main/java/com/dang1000/releaspoon/adapter/FeedAdapter.kt
+++ ReleaeSpoon/app/src/main/java/com/dang1000/releaspoon/adapter/FeedAdapter.kt
@@ -1,6 +1,9 @@
 package com.dang1000.releaspoon.adapter
 
 import ArtifactFeed
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -23,12 +26,21 @@ class FeedAdapter(
         return FeedViewHolder(binding)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: FeedViewHolder, position: Int) {
         val item = items[position]
         holder.binding.let {
             it.tvDoc.text = item.text
             it.tvVersion.text = item.version
             it.tvLibraryName.text = name
+            it.btnLink.setOnClickListener {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(item.links.first()))
+                it.context.startActivity(intent)
+            }
+            it.ivBookmark.setOnClickListener {
+                it.isSelected = !it.isSelected
+            }
+            it.cpImpact.text = "impact : ${item.impact}"
         }
     }
 
